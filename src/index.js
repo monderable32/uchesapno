fa72931ddb2c8bceb987e580cab21dd049d9ecb6
Monderable <mehmet.onder@dotto.com.tr>
2018-09-04 14:18:44 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
 import React, { Component } from 'react'
-import {form, FormGroup, FormControl } from 'react-bootstrap'
+import { form, FormGroup, FormControl, InputGroup, Button } from 'react-bootstrap'
+//import ReactDOM from 'react-dom'
+export default class UcHesapNo extends Component {
+    constructor(props, context) {
+        super(props, context);
 
-export default class ucHesapNo extends Component
-{
+        this.handleChange = this.handleChange.bind(this);
+
+        this.state = {
+            value: ''
+        };
+    }
+
+    handleChange(e) {
+        //this.setState({ value: e.target.value });
+    }
     render() {
-        return ( 
+        return (
             <form>
-            <FieldGroup
-              id="formControlsText"
-              type="text"
-              placeholder="Müşteri Numarası"
-            />
+                <FormGroup>
+                    <InputGroup>
+                        <FormControl
+                            type="text"
+                            value={this.state.value}
+                            placeholder="Enter text"
+                            onChange={this.handleChange}
+                        />
+                        <InputGroup.Button>
+                            <Button>Search</Button>
+                        </InputGroup.Button></InputGroup>
+                </FormGroup>
             </form>
         );
     }
 }
+/*
+ReactDOM.render(
+    <UcHesapNo/>,
+    document.getElementById('root')
+);*/
